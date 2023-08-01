a07665777ad95eb80d5f190a193a9b63cf6b96e1
rary <rary@rary.com>
2023-08-01 10:44:54 +0900

    update

--- .zshrc
+++
@@ -1,3 +0,0 @@
-export PS1="%F{green}%d %% %f"
-alias ll='ls -la'
-alias vim='nvim'
--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
 ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# append git prompt settings to bashrc
+cd ~/.local
+GIT_VERSION=$(git --version | cut -d' ' -f3)
+if [ ! -f git-prompt.sh ]; then
+    wget https://raw.githubusercontent.com/git/git/v$GIT_VERSION/contrib/completion/git-prompt.sh
+fi
+if [ ! -f git-completion.bash ]; then
+    wget https://raw.githubusercontent.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash
+fi
+
+cat << 'EOF' >> ~/.bashrc
+
+# git
+if [ -f ~/.local/git-completion.bash ]; then
+    source ~/.local/git-completion.bash
+fi
+if [ -f ~/.local/git-prompt.sh ]; then
+    source ~/.local/git-prompt.sh
+fi
+
+GIT_PS1_SHOWDIRTYSTATE=true
+export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w$(__git_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
+EOF
+
